c29c1a6397c3243c56dae8a0b4de6d46f75b88a6
JonMercer <mercer.jon@gmail.com>
2015-09-07 15:06:28 -0700

    Taking a string from a text and putting it in a label when button is pressed

--- BasicInteraction/BasicInteraction/ViewController.m
+++ BasicInteraction/BasicInteraction/ViewController.m
@@ -22,6 +22,21 @@
 //return type means this method can be used in interface builder of xcode
 //IBAction is pretty much void for now
 - (IBAction)changeLabel:(id)sender {
+    
+    //but the getter method has been generated in the class
+//    NSString *contents = [_simpleTextField text];
+    // another way of doing it
+//    NSString *contents = [[self simpleTextField] text];
+    // Yet another way (but no longes supported it seems like
+//    NSString *contents = simpleTextField.text;
+    
+    
+    
+    NSString *contents = [[self simpleTextField] text];
+    
+    NSString *message = [NSString stringWithFormat:@"Hello %@",contents];
+    [self.simpleLabel setText:message];
+    
 }
 
 - (void)viewDidLoad {
