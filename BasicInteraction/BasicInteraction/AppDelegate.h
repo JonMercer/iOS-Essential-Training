c27cea1dc76e0c9a1692df77429a77a9fd6d7098
JonMercer <mercer.jon@gmail.com>
2015-09-07 14:45:04 -0700

    BLANK BasicInteraction

---
+++ BasicInteraction/BasicInteraction/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BasicInteraction
+//
+//  Created by Odin on 2015-09-07.
+//  Copyright (c) 2015 Jon Mercer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BasicInteraction/BasicInteraction/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BasicInteraction
+//
+//  Created by Odin on 2015-09-07.
+//  Copyright (c) 2015 Jon Mercer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
