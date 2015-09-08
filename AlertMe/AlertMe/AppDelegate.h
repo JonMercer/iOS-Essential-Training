96e6135bb7c9bcb00b40f2aeb54633dc3a818902
JonMercer <mercer.jon@gmail.com>
2015-09-08 11:37:45 -0700

    NEW project. AlertMe

---
+++ AlertMe/AlertMe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlertMe
+//
+//  Created by Odin on 2015-09-08.
+//  Copyright (c) 2015 Jon Mercer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AlertMe/AlertMe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AlertMe
+//
+//  Created by Odin on 2015-09-08.
+//  Copyright (c) 2015 Jon Mercer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
